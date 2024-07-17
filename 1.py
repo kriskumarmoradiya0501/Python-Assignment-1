d3cc7914b8a1d5a1b341195eb9cd70321244e987
krish moradiya <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2024-07-17 14:26:05 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+number1=float(input("Enter number1 : "))
+number2=float(input("Enter number2 : "))
+
+print("Number1 = ",number1," Number2 = ",number2)
+print(number1," + ",number2," = ",(number1+number2))
+print(number1," - ",number2," = ",(number1-number2))
+print(number1," / ",number2," = ",(number1/number2))
+print(number1," // ",number2," = ",(number1//number2))
+print(number1," * ",number2," = ",(number1*number2))
+print(number1," ** ",number2," = ",(number1**number2))
+print(number1," % ",number2," = ",(number1%number2))
---
+++ 2.py
@@ -0,0 +1,37 @@
+#f_name=input("Enter First Name : ")
+#by=int(input("Enter Your Born year : "))
+# l_name=input("Enter Last Name : ")
+# city=input("Enter City : ")
+# mobil=int(input("Enter mobile number : "))
+# email=input("Enter Email Id : ")
+#age = 2024-by
+#print("name : ",f_name)
+#print("age : ",age)
+
+#print("Last name : ",l_name)
+#print("City : ",city)
+#print("Mobile number : ",mobil)
+#print("Email ID : ",email)
+
+#eng=float(input("Enter marks of English : "))
+#guj=float(input("Enter marks of Gujrati : "))
+#com=float(input("Enter marks of Computer : "))
+#sci=float(input("Enter marks of Science : "))
+#ss=float(input("Enter marks of Social science : "))
+
+#print("English = ",eng)
+#print("Gujrati = ",guj)
+#print("Computer = ",com)
+#print("Science = ",sci)
+#print("Social Science = ",ss)
+#print("Percentage = ",((eng+guj+com+sci+ss)/5.0))
+
+emp_id=input("Enter Employee Id = ")
+name=input("Enter Employee name= ")
+experience=int(input("Enter Employee Experience number = "))
+c_salary=float(input("Enter Employee Current salary = "))
+i_sal =(c_salary*0.10)
+
+print("Incremented salary is ",i_sal);
+c_salary+=i_sal
+print("Updated salary is ",c_salary)
